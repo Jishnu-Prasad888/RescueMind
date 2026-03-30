72fff5129b949548c0397f2d1c2f33c9a0241744
vm1-23 <173900329+vm1-23@users.noreply.github.com>
2026-03-30 10:45:12 +0530

    Dockerized

--- graph_server/rescuemind-api/internal/server/server.go
+++ graph_server/rescuemind-api/internal/server/server.go
@@ -2,14 +2,19 @@ package server
 
 import (
 	"log"
+	"os"
 	"rescuemind-api/internal/routing"
 )
 
 var RoutingClient *routing.RoutingClient
 
 func InitServices() {
+	grpcURL := os.Getenv("GRPC_SERVER_URL")
+	if grpcURL == "" {
+		grpcURL = "localhost:50051"
+	}
 
-	client, err := routing.NewRoutingClient("localhost:50051")
+	client, err := routing.NewRoutingClient(grpcURL)
 	if err != nil {
 		log.Fatal("Failed to connect to routing engine:", err)
 	}
